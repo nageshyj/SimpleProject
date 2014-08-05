8a9f060a98443d9c79ce1b224604e7612cb3bd1b
Nagesha Yellur JAYARAMA <nagesha.jayarama@amadeus.com>
2014-08-05 15:33:17 +0530

    Simple Project

---
+++ com.amadeus.simple.project.test/src/com/amadeus/simple/project/test/TestCalculator.java
@@ -0,0 +1,39 @@
+package com.amadeus.simple.project.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.amadeus.simple.project.Calculator;
+
+/**
+ * Calculator Test
+ * 
+ * @author njayarama
+ *
+ */
+public class TestCalculator {
+	
+
+	@Test
+	public void testAddValues() {
+		
+		Calculator calculator = new Calculator();
+		
+		long result = calculator.addValues(4, 5);
+		Assert.assertEquals(9, result);
+		
+		result = calculator.addValues(5, 4);
+		Assert.assertEquals(9, result);
+	}
+
+	@Test
+	public void testMultiplyValues() {
+		Calculator calculator = new Calculator();
+		
+		long result = calculator.multiplyValues(4, 5);
+		Assert.assertEquals(20, result);
+		
+		result = calculator.multiplyValues(5, 4);
+		Assert.assertEquals(20, result);
+	}
+}
---
+++ com.amadeus.simple.project/src/com/amadeus/simple/project/Calculator.java
@@ -0,0 +1,32 @@
+package com.amadeus.simple.project;
+
+/**
+ * Calculator.
+ * 
+ * @author njayarama
+ *
+ */
+public class Calculator {
+
+	/**
+	 * Add
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public long addValues(int a, int b) {
+		return a + b;
+	}
+	
+	/**
+	 * Multiply 
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public long multiplyValues(int a, int b) {
+		return a * b;
+	}
+}

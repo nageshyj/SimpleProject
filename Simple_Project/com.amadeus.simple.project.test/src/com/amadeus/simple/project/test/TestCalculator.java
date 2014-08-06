a894320fe908c16fced3028fe9e58a1a4682974e
Nagesha Yellur JAYARAMA <nagesha.jayarama@amadeus.com>
2014-08-06 16:26:51 +0530

    Latest Gradle added

--- Simple_Project/com.amadeus.simple.project.test/src/com/amadeus/simple/project/test/TestCalculator.java
+++ Simple_Project/com.amadeus.simple.project.test/src/com/amadeus/simple/project/test/TestCalculator.java
@@ -1,6 +1,9 @@
 package com.amadeus.simple.project.test;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import com.amadeus.simple.project.Calculator;
 
 
 /**
@@ -15,23 +18,23 @@ public class TestCalculator {
 	@Test
 	public void testAddValues() {
 		
-		/*Calculator calculator = new Calculator();
+		Calculator calculator = new Calculator();
 		
 		long result = calculator.addValues(4, 5);
 		Assert.assertEquals(9, result);
 		
 		result = calculator.addValues(5, 4);
-		Assert.assertEquals(9, result);*/
+		Assert.assertEquals(9, result);
 	}
 
 	@Test
 	public void testMultiplyValues() {
-		/*Calculator calculator = new Calculator();
+		Calculator calculator = new Calculator();
 		
 		long result = calculator.multiplyValues(4, 5);
 		Assert.assertEquals(20, result);
 		
 		result = calculator.multiplyValues(5, 4);
-		Assert.assertEquals(20, result);*/
+		Assert.assertEquals(20, result);
 	}
 }
